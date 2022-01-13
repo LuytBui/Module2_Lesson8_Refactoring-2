feaf3a2f74b0d44486adba45aa5e952b77739214
LuytBui <luytbq43@gmail.com>
2022-01-13 10:59:47 +0700

    done

--- src/com/codegym/FizzBuzz.java
+++ src/com/codegym/FizzBuzz.java
@@ -1,16 +1,26 @@
 package com.codegym;
 
 public class FizzBuzz {
-    public static String fizzBuzz(int number){
-        if(number % 3 == 0 && number % 5 == 0)
-            return "FizzBuzz";
 
-        if(number % 3 == 0)
-            return "Fizz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String FIZZ_BUZZ ="FizzBuzz";
 
-        if(number % 5 == 0)
-            return "Buzz";
+    public static String fizzBuzz(int number) {
+        boolean divisibleByThree = number % 3 == 0;
+        boolean divisibleByFive = number % 5 == 0;
+        boolean divisibleByFifteen = divisibleByThree && divisibleByFive;
 
-        return number + "";
+        String result = number + "";
+
+        if (divisibleByFifteen) {
+            result = FIZZ_BUZZ;
+        } else if (divisibleByThree) {
+            result = FIZZ;
+        } else if (divisibleByFive == 0){
+            result = BUZZ;
+        }
+
+        return result;
     }
 }
